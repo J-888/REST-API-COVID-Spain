6ae5964c24118fc2e011d46aafc9b9a8dc766ce1
José Miguel Tajuelo Garrigós <josemigueltajuelo@gmail.com>
2020-04-04 19:12:08 +0200

    Bug fixed in urls: missing import
    Co-authored-by: Von-Omega <14225222+Von-Omega@users.noreply.github.com>

--- COVIDSpain/urls.py
+++ COVIDSpain/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
 	2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+# from django.urls import path
+from django.conf.urls import url, include
 
 urlpatterns = [
-	path('admin/', admin.site.urls),
+	#path('admin/', admin.site.urls),
 	url(r'^', include('reports.urls')),
 ]
