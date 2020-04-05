831e859587f45f0e217870d01cdbdd629eb2d03e
José Miguel Tajuelo Garrigós <josemigueltajuelo@gmail.com>
2020-04-05 23:47:32 +0200

    Set to production
    Co-authored-by: Von-Omega <14225222+Von-Omega@users.noreply.github.com>

--- COVIDSpain/settings.py
+++ COVIDSpain/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wpu@gn5u!bbf#r3wb_ut3xdyde1vm39znmenc_73te*r))5cz6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -128,3 +128,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
